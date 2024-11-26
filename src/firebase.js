0342ef43a1e15ac7ba4d0e5bf2ef63aaac7f243b
Chkjaer <chkjaer@uw.edu>
2024-11-26 13:24:33 -0800

    no more secrets

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyClA3NPBpx757HyxLc8jKJuNhbD16LfHNI",
-  authDomain: "chord-app-97a70.firebaseapp.com",
-  projectId: "chord-app-97a70",
-  storageBucket: "chord-app-97a70.firebasestorage.app",
-  messagingSenderId: "257084500842",
-  appId: "1:257084500842:web:e1786870c7496bf6ac1313"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClA3NPBpx757HyxLc8jKJuNhbD16LfHNI",
+  authDomain: "chord-app-97a70.firebaseapp.com",
+  projectId: "chord-app-97a70",
+  storageBucket: "chord-app-97a70.firebasestorage.app",
+  messagingSenderId: "257084500842",
+  appId: "1:257084500842:web:e1786870c7496bf6ac1313"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
